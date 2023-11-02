9eab74458e479c3d37563e2bf9051c471b1e44b9
oussamaef06 <oussamaelfarihi@gmail.com>
2023-11-02 23:17:08 +0100

    second commit

--- js/cars.js
+++ js/cars.js
@@ -1,11 +1,17 @@
 let productCars = [ 
     { id: 0, image: 'img/featured1.png', name: 'Tesla',model:'model X' ,price: 120.000, },
-    { id: 1, image: 'img/featured2.png', name: 'Porsche',model:'Boxter 987', price: 160.000, }, 
+    { id: 1, image: 'img/featured5.png', name: 'Porsche',model:'Boxter 987', price: 160.000, }, 
     { id: 2, image: 'img/featured3.png', name: 'Audi',model:'E-tron ' ,price: 130.00, },
-    { id: 3, image: 'img/featured4.png', name: 'Tesla',model:'model 3', price: 100.000, },
+    { id: 3, image: 'img/featured2.png', name: 'Tesla',model:'model 3', price: 100.000, },
     { id: 4, image: 'img/audi rs6.png', name: 'Audi',model:'rs6' ,price: 220.000, },
     { id: 5, image: 'img/718 cayma.png', name: 'Porsche',model:'718 cayma', price: 260.000, }, 
-    { id: 6, image: 'img/audi q8.png', name: 'Audi',model:'Q8 ' ,price: 230.878, }
+    { id: 6, image: 'img/audi q8.png', name: 'Audi',model:'Q8 ' ,price: 230.878, },
+    { id: 7, image: 'img/g63.png', name: 'mercedes',model:'g63 ' ,price: 230.500, },
+    { id: 7, image: 'img/amg gtr.png', name: 'mercedes',model:'amg gtr ' ,price: 450.500, },
+    { id: 7, image: 'img/bmw x6.png', name: 'bmw',model:'X6' ,price: 130.500, },
+    { id: 7, image: 'img/bmw s5.png', name: 'bmw',model:'Serie 5' ,price: 230.500, },
+    { id: 7, image: 'img/eventador.png', name: 'lambo',model:'eventador' ,price: 700.500, },
+    { id: 7, image: 'img/urus.png', name: 'lambo',model:'urus' ,price: 550.500, }
 ];
 
 
@@ -63,6 +69,7 @@ function cars() {
         const productItem = document.createElement('div');
         productItem.classList.add('col-md-4', 'mb-3');
         productItem.innerHTML = `
+        <a href="carsInfo.html">
         <article class="featured__card"  mix tesla>
         <div class="shape__smaller">
             <h1 class="featured__title">${name}</h1>
@@ -72,7 +79,7 @@ function cars() {
             <img src="${image}"  class="featured__img" onclick="navigateToCart(${id})">
             
             <h3 class="featured__price">$${price}</h3>
-
+            </a>
             <button class="button featured__button" onclick="addtocart(${index})">
                 <i class="ri-shopping-bag-2-line"></i>
             </button>
